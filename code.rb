7c45f77dfb1d7023b89a9252220b457d73fe44bc
2003cah <2003cah@gmail.com>
2017-01-07 18:41:20 -0500

    Maybe fix

--- code.rb
+++ code.rb
@@ -169,12 +169,12 @@ bot.command([:cmds, :commands], chain_usable: false, max_args: 0) do |event|
 puts "^commands | Command ran by #{event.user.name}\##{event.user.discriminator} (ID: #{event.user.id}) on server #{event.server.name} (ID: #{event.server.id})"
 end
 
-bot.command(:feedback, min_args: 1) do |_event, *args|
+bot.command(:feedback, min_args: 1) do |event, *args|
   _event.message.delete
   bot.send_message(252239053712392192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of `#{event.server.name}` (Server ID: #{event.server.id}).
 
 *#{args.join(' ')}*")
-  m = (_event.respond "Radical! Feedback sent.")
+  m = (event.respond "Radical! Feedback sent.")
 puts "^feedback | Command ran by #{event.user.name}\##{event.user.discriminator} (ID: #{event.user.id}) on server #{event.server.name} (ID: #{event.server.id})"
   sleep 5
   m.delete
