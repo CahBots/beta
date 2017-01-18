f9fb6ab1ce149239005098b5e1c3b90bce2559c1
2003cah <2003cah@gmail.com>
2017-01-18 15:46:38 -0500

    Works but doesn't return error in DM, maybe fix

--- code.rb
+++ code.rb
@@ -180,13 +180,14 @@ end
 bot.command(:feedback, min_args: 1) do |event, *args|
   if event.channel.pm? == true
     "Sorry, you can't send feedback via PM just yet!"
+    puts "^feedback | Command ran by #{event.user.name}\##{event.user.discriminator} (ID: #{event.user.id}) ran in a DM"
   break
   else
     event.message.delete
     bot.send_message(252239053712392192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of `#{event.server.name}` (Server ID: #{event.server.id}).
 *#{args.join(' ')}*")
     m = (event.respond "Radical! Feedback sent.")
-puts "^feedback | Command ran by #{event.user.name}\##{event.user.discriminator} (ID: #{event.user.id}) on server #{event.server.name} (ID: #{event.server.id})"
+    puts "^feedback | Command ran by #{event.user.name}\##{event.user.discriminator} (ID: #{event.user.id}) on server #{event.server.name} (ID: #{event.server.id})"
     sleep 5
     m.delete
   end
