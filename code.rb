7d135e2c9f8373898acce9753ab9a56b8427d411
2003cah <2003cah@gmail.com>
2017-01-18 14:58:28 -0500

    No shut up github

--- code.rb
+++ code.rb
@@ -176,15 +176,20 @@ puts "^commands | Command ran by #{event.user.name}\##{event.user.discriminator}
 end
 
 bot.command(:feedback, min_args: 1) do |event, *args|
+if event.channel.pm? == true
+  "Sorry, you can't send feedback via PM just yet!"
+  break
+end
+else
   event.message.delete
   bot.send_message(252239053712392192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of `#{event.server.name}` (Server ID: #{event.server.id}).
-
 *#{args.join(' ')}*")
   m = (event.respond "Radical! Feedback sent.")
 puts "^feedback | Command ran by #{event.user.name}\##{event.user.discriminator} (ID: #{event.user.id}) on server #{event.server.name} (ID: #{event.server.id})"
   sleep 5
   m.delete
 end
+
 bot.message(with_text: '<@267104172049039373> help') do |event|
   event << ' woahdude, you looking for help? Well, here\'s what you need to know.'
   event << ' For a list of commands, you can do `B^cmds`, for info about CahBot, do `B^info`'
