b16bff5c09cd3d94dd08394e80aff0607bef50d3
2003cah <2003cah@gmail.com>
2017-01-19 18:48:48 -0500

    Sending feedback via DM...?

--- code.rb
+++ code.rb
@@ -179,9 +179,13 @@ end
 
 bot.command(:feedback, min_args: 1) do |event, *args|
   if event.channel.pm? == true
-    event.respond "Sorry, you can't send feedback via PM just yet!"
-    puts "^feedback | Command ran by #{event.user.name}\##{event.user.discriminator} (ID: #{event.user.id}) ran in a DM"
-  break
+    bot.send_message(252239053712392192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From a DM.
+
+*#{args.join(' ')}*")
+    m = (event.respond "Radical! Feedback sent.")
+    sleep 5
+    m.delete
+  end
   else
     event.message.delete
     bot.send_message(252239053712392192, "New Feedback from `#{event.user.name}`\##{event.user.discriminator}. ID: #{event.user.id}. From the land of `#{event.server.name}` (Server ID: #{event.server.id}).
