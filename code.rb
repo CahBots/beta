0023816ae4bbda6cc7df08ea5a9d65eb03c0f839
2003cah <2003cah@gmail.com>
2017-02-10 15:35:22 -0500

    Because why not

--- code.rb
+++ code.rb
@@ -34,6 +34,7 @@ end
 bot.command(:restart, help_available: false) do |event|
   if event.user.id == 228290433057292288
     begin
+      event.respond ["Into the ***fuuuutttttuuuuurrrreeee***", "Please wait...", "How about n—", "Can do :thumbsup::skin-tone-1:", "Pong! Hey, that took... Oh wait, wrong command", "Ask again at a later ti—"].sample
       exec("bash restart.sh")
     end
   else
