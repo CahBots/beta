d7de526cd9aeff457960dd09d404aa9fd91d669b
2003cah <2003cah@gmail.com>
2017-02-20 15:55:23 -0500

    CahBotBeta

--- code.rb
+++ code.rb
@@ -57,7 +57,7 @@ bot.command(:set, help_available: false) do |event, action, *args|
       when 'avatar'
         open("#{args.join}") { |pic| event.bot.profile.avatar = pic }
       when 'username'
-        bot.profile.username=("#{args.join}")
+        bot.profile.username=("#{args.join(' ')}")
       else
         "I don't know what to do!"
       end
